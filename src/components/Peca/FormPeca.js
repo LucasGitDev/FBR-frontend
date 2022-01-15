eef8f52a9bc728aaaf8657d353a910cf66b9578e
Lucas Teles <ls5651405@gmail.com>
2022-01-15 01:06:44 -0300

    feat: add function to create new Peças

--- src/components/Peca/FormPeca.js
+++ src/components/Peca/FormPeca.js
@@ -1,66 +1,170 @@
 import { TextField } from "@mui/material/";
 import React, { useEffect, useState } from "react";
-import { Button } from "@mui/material/";
+import { Button, Alert } from "@mui/material/";
 import "../../assets/CSS/FormPeca.css";
 import Api from "../../service/api";
 
-export const FormPeca = (props) => {
+const pecaModel = {
+  nome: "",
+  descricao: "",
+  codigo: "",
+  quantidade: "",
+  descricaoCompleta: "",
+  valorEntrada: "",
+  valorSaida: "",
+  quantidadeMinima: "",
+  desconto: "",
+  localizacao: "",
+  tipo: "",
+  data_ultima_remessa: "",
+};
 
-    const [id, setId] = useState(Number(props.id));
-    const [isCreate, setIsCreate] = useState(id === -1);
-    const [peca, setPeca] = useState({})
+export const FormPeca = (props) => {
+  const [id, setId] = useState(Number(props.id));
+  const [isCreate, setIsCreate] = useState(id === -1);
+  const [peca, setPeca] = useState(
+    id !== -1
+      ? Api.getPeca(id).then((response) => {
+          setPeca(response);
+        })
+      : pecaModel
+  );
+
+  const incompleto = false
 
   useEffect(() => {
-    if(id !== -1) {
-        Api.getPeca(id).then((response) => {
-            setPeca(response);
+    peca.data_ultima_remessa = '2020-01-01'
+  }, [peca]);
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setPeca({
+      ...peca,
+      [evt.target.name]: value,
+    });
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    if(peca !== pecaModel) {
+
+      peca.quantidade = Number(peca.quantidade)
+      peca.quantidadeMinima = Number(peca.quantidadeMinima)
+      peca.valorEntrada = Number(peca.valorEntrada)
+      peca.valorSaida = Number(peca.valorSaida)
+      peca.desconto = Number(peca.desconto)
+
+
+      if (isCreate) {
+        Api.createPeca(peca).then((response) => {
+          console.log(response);
         });
+      } else {
+        Api.updatePeca(peca).then((response) => {
+          console.log(response);
+        });
+      }
     } else {
-        setPeca({
-            nome: "a",
-            descricao: "",
-            codigo: 0,
-            quantidade: 0,
-            descricao_completa: '',
-            valor_entrada: 0,
-            valor_saida: 0,
-            quantidade_minima: 0,
-            desconto: 0,
-            localizacao: '',
-            tipo: '',
-            data_ultima_remessa: '',
-        });
+      incompleto = true
     }
-  }, [id]);
-
-
+  }
 
   return (
     <form>
       <div>
         <div className="contain">
-          <TextField label={"Nome"} required={isCreate} />
-          <TextField label={"Código"} required={isCreate} />
-          <TextField label={"Descrição simples"} required={isCreate} />
+          <TextField
+            label={"Nome"}
+            name="nome"
+            value={peca.nome}
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Código"}
+            name="codigo"
+            value={peca.codigo}
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Descrição simples"}
+            value={peca.descricao}
+            name="descricao"
+            onChange={handleChange}
+            required={isCreate}
+          />
         </div>
         <div className="contain">
-          <TextField label={"Posição nas prateleiras"} required={isCreate} />
-          <TextField label={"Tipo de peça"} required={isCreate} />
-          <TextField label={"Quantidade em Estoque"} required={isCreate} />
-          <TextField label={"Quantidade miníma"} required={isCreate} />
+          <TextField
+            label={"Posição nas prateleiras"}
+            value={peca.localizacao}
+            name="localizacao"
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Tipo de peça"}
+            value={peca.tipo}
+            name="tipo"
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Quantidade em Estoque"}
+            type={isCreate ? "number" : "text"}
+            value={peca.quantidade}
+            name="quantidade"
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Quantidade miníma"}
+            type={isCreate ? "number" : "text"}
+            value={peca.quantidadeMinima}
+            name="quantidadeMinima"
+            onChange={handleChange}
+            required={isCreate}
+          />
         </div>
         <div className="contain">
-          <TextField label={"Valor de Custo"} required={isCreate} />
-          <TextField label={"Valor de Venda"} required={isCreate} />
-          <TextField label={"Desconto à vista"} />
+          <TextField
+            label={"Valor de Custo"}
+            type={isCreate ? "number" : "text"}
+            name="valorEntrada"
+            value={peca.valorEntrada}
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Valor de Venda"}
+            type={isCreate ? "number" : "text"}
+            value={peca.valorSaida}
+            name="valorSaida"
+            onChange={handleChange}
+            required={isCreate}
+          />
+          <TextField
+            label={"Desconto à vista"}
+            type={isCreate ? "number" : "text"}
+            value={peca.desconto}
+            name="desconto"
+            onChange={handleChange}
+          />
         </div>
       </div>
       <div>
-        <TextField label={"Descrição"} />
+        <TextField
+          label={"Descrição"}
+          value={peca.descricaoCompleta}
+          name="descricaoCompleta"
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <Button type="submit">Cadastrar</Button>
+        <Button type="submit" onClick={handleSubmit}>Cadastrar</Button>
         <Button variant={"outlined"}>Reset</Button>
+        {/* <Alert severity="error" hidden={incompleto}>Preencha todos os campos</Alert> */}
       </div>
     </form>
   );
--- src/service/api.js
+++ src/service/api.js
@@ -1,24 +1,37 @@
 import axios from "axios";
 import pecas from "../assets/pecas_teste";
-const url = `https://fbr-sys-backend.herokuapp.com` 
-// url = "http://localhost:3333"
-export default class Api { 
+// const url = `https://fbr-sys-backend.herokuapp.com`
+const url = `http://localhost:3333`;
+export default class Api {
   static api = axios.create({
     baseURL: url,
   });
 
   static getPecas() {
-    return this.api.get(`/pecas`).then((response) => {
-      return response.data;
-    }).catch((error) => {
-      console.log(error);
-      return pecas;
-    });
+    return this.api
+      .get(`/pecas`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        return pecas;
+      });
   }
-
 
   static async searchPecas(search) {
     const response = await this.api.get(`/pecas/search?search=${search}`);
+    return response.data;
+  }
+
+  static async createPeca(peca) {
+    console.log(peca);
+    const response = await this.api.post(`/pecas`, peca);
+    return response.data;
+  }
+
+  static async updatePeca(peca) {
+    const response = await this.api.put(`/pecas/${peca.id}`, peca);
     return response.data;
   }
 }
